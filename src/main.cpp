4acee75118cea802cf792f91a627e3db56a948d7
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-03-07 21:26:25 +0400

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,9 @@
 #include "AccumulatedEvents.hpp"
 #include "KeypointsDetection.hpp"
-#include "geometry_msgs/PointStamped.h"
-#include "sensor_msgs/Image.h"
 #include <ros/ros.h>
 #include <opencv2/core/types.hpp>
-#include <image_transport/image_transport.h>
-#include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include "sensor_msgs/Image.h"
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/types.hpp>
-#include <opencv2/features2d.hpp>
+
+
 
 int main(int argc, char** argv)
 {
