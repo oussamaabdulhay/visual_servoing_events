db4c8e79fad7597c04a059824bea55089e81114e
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-04-27 14:09:40 +0400

    .

--- src/KeypointsDetection.cpp
+++ src/KeypointsDetection.cpp
@@ -9,7 +9,7 @@ KeypointsDetection::KeypointsDetection(ros::NodeHandle &t_nh):it_(nh_)
 
     params.filterByArea = true;
     params.minArea = 400;
-    params.maxArea = 2000;
+    params.maxArea = 2500;
 
     // Filter by Circularity
     params.filterByCircularity = true;
