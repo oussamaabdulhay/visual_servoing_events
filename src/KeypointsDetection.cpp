37e14562376ba971d5e2ae31e5c0601708d23a5f
oussamaabdulhay <oussama.abdulhay@gmail.com>
2021-06-17 10:33:18 +0400

    .

--- src/KeypointsDetection.cpp
+++ src/KeypointsDetection.cpp
@@ -63,8 +63,8 @@ void KeypointsDetection::findCenter(cv::Mat* EventsImage, std_msgs::Header Event
         center_point.x = keypoints[0].pt.x;
         center_point.y = keypoints[0].pt.y;
         
-        pixel_pos.point.x = center_point.x-175.6;
-        pixel_pos.point.y = center_point.y-133.9;
+        pixel_pos.point.x = center_point.x-172.62;
+        pixel_pos.point.y = center_point.y-126.6;
         pixel_pos.point.z = 0;
         pixel_pos.header = EventImageHeader;
     
@@ -108,8 +108,8 @@ void KeypointsDetection::findCenter(cv::Mat* EventsImage, std_msgs::Header Event
   }
     
   
-    // cv::imshow("im_with_keypoints", im_with_keypoints); 
-    // cv::waitKey(1);  
+    cv::imshow("im_with_keypoints", im_with_keypoints); 
+    cv::waitKey(1);  
 
 }
 }
